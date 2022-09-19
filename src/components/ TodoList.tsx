b684f47b4d86a75b080a1e3c94fcbd9481662e95
kira-0521 <kiratanaka0521@gmail.com>
2022-09-19 09:43:50 +0900

    [ add ] 分岐

--- src/components/ TodoList.tsx
+++ src/components/ TodoList.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react'
+import { isEmpty } from 'lodash'
+
 import { useTodoStore } from '../store/todoStore'
 
 export const TodoList = () => {
@@ -9,25 +11,37 @@ export const TodoList = () => {
     fetchTodos()
   }, [])
 
+  const ErrorMessage = () => (
+    <p className="text-red-500">Todoが存在しません。</p>
+  )
+
+  const TodoLists = () => {
+    return (
+      <>
+        {todos.map((todo) => (
+          <li
+            key={todo.id}
+            className="flex gap-x-2 items-center border-dotted border-b py-2 mt-2"
+          >
+            <span>{todo.id}. </span>
+            <span className="text-gray-500">{todo.title}</span>
+            <button className="bg-gray-400 hover:bg-gray-300 text-white rounded px-2 py-1">
+              編集
+            </button>
+            <input
+              type="checkbox"
+              name="todo-completed"
+              id={todo.id.toString()}
+            />
+          </li>
+        ))}
+      </>
+    )
+  }
+
   return (
     <ul className="flex gap-3 flex-col">
-      {todos.map((todo) => (
-        <li
-          key={todo.id}
-          className="flex gap-x-2 items-center border-dotted border-b py-2 mt-2"
-        >
-          <span>{todo.id}. </span>
-          <span className="text-gray-500">{todo.title}</span>
-          <button className="bg-gray-400 hover:bg-gray-300 text-white rounded px-2 py-1">
-            編集
-          </button>
-          <input
-            type="checkbox"
-            name="todo-completed"
-            id={todo.id.toString()}
-          />
-        </li>
-      ))}
+      {isEmpty(todos) || todos[0].id === 0 ? <ErrorMessage /> : <TodoLists />}
     </ul>
   )
 }

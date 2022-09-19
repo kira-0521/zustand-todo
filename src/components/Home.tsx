eb02e673f5fb2aa3b4db3dc7123802cdfc5b5b9c
kira-0521 <kiratanaka0521@gmail.com>
2022-09-19 10:12:20 +0900

    [ add ] padding

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,10 +3,10 @@ import { TodoList } from './ TodoList'
 
 export const Home = () => {
   return (
-    <>
+    <div className="p-6">
       <TodoForm />
       <br />
       <TodoList />
-    </>
+    </div>
   )
 }

c5a69da1bbdb87806fcfe3a5a063df3e9929333b
kira-0521 <kiratanaka0521@gmail.com>
2022-09-19 11:56:54 +0900

    [ update ] 自分のTodoだけ編集可能

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react'
 import { isEmpty } from 'lodash'
 
 import { useTodoSlice } from '../store/todoSlice'
+import { useUserSlice } from '../store/userSlice'
 
 export const TodoList = () => {
   const todos = useTodoSlice((state) => state.todos)
   const setEditedTodo = useTodoSlice((state) => state.setEditedTodo)
   const fetchTodos = useTodoSlice((state) => state.fetchTodos)
+  const loginUser = useUserSlice((state) => state.user)
 
   useEffect(() => {
     fetchTodos()
@@ -19,19 +21,21 @@ export const TodoList = () => {
   const TodoLists = () => {
     return (
       <>
-        {todos.map((todo) => (
+        {todos.map((todo, index) => (
           <li
             key={todo.id}
             className="flex gap-x-2 items-center border-dotted border-b py-2 mt-2"
           >
-            <span>{todo.id}. </span>
+            <span>{index + 1}. </span>
             <span className="text-gray-500">{todo.title}</span>
-            <button
-              onClick={() => setEditedTodo(todo.id)}
-              className="bg-gray-400 hover:bg-gray-300 text-white rounded px-2 py-1"
-            >
-              編集
-            </button>
+            {todo.userId === loginUser.id && (
+              <button
+                onClick={() => setEditedTodo(todo.id)}
+                className="bg-gray-400 hover:bg-gray-300 text-white rounded px-2 py-1"
+              >
+                編集
+              </button>
+            )}
             <input
               type="checkbox"
               name="todo-completed"

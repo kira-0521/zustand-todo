29bd43957870998f30847e8fa1d6d934a1f9fa13
kira-0521 <kiratanaka0521@gmail.com>
2022-09-22 19:42:33 +0900

    [ add ] not exists user

--- src/libs/api/error.ts
+++ src/libs/api/error.ts
@@ -1,14 +1,19 @@
 export const ERROR_CODES = {
-"NO_ERROR": {
-  code: 1000,
-    message: ''
+  NO_ERROR: {
+    code: 1000,
+    message: '',
   },
-  "BAD_REQUEST": {
+  NOT_EXISTS_USER: {
+    code: 2000,
+    message: 'ユーザーが存在しません。',
+  },
+  BAD_REQUEST: {
     code: 4000,
-    message: '不正なリクエストです。もう一度やり直してください。'
+    message: '不正なリクエストです。もう一度やり直してください。',
   },
-  "INTERNAL_SERVER_ERROR": {
+  INTERNAL_SERVER_ERROR: {
     code: 5000,
-    message: 'サーバー内部で何らかのエラーが発生しました。ページを再読み込みしてください。'
+    message:
+      'サーバー内部で何らかのエラーが発生しました。ページを再読み込みしてください。',
   },
 }

d19d34fe940c64eedd6b2eff730b1745649813e0
kira-0521 <kiratanaka0521@gmail.com>
2022-09-25 14:37:53 +0900

    [ add ] edit todo

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,4 +1,18 @@
+import { ChangeEvent, useCallback } from 'react'
+import { useRecoilState } from 'recoil'
+
+import { editedTodoAtom } from '../store/todoStates'
+
 export const TodoForm = () => {
+  const [editedTodo, setEditedTodo] = useRecoilState(editedTodoAtom)
+
+  const onChangeTodo = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEditedTodo((todo) => ({
+      ...todo,
+      title: e.target.value,
+    }))
+  }, [])
+
   return (
     <div className="flex items-center justify-center gap-x-2">
       <label
@@ -8,6 +22,8 @@ export const TodoForm = () => {
         <input
           type="text"
           id="title"
+          onChange={onChangeTodo}
+          value={editedTodo.title}
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
         />
       </label>

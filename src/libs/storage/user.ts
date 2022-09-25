fe63d995efb51755f7e41ef996d26b6f4dfc7fa9
kira-0521 <kiratanaka0521@gmail.com>
2022-09-25 12:54:46 +0900

    [ add ] storage for loginuser

---
+++ src/libs/storage/user.ts
@@ -0,0 +1,15 @@
+import { isNil } from 'lodash'
+import { UserValueAtom } from '../../store/userStates'
+import { CURRENT_USER_STORAGE_KEY } from '../const'
+
+export const getCurrentUserByStorage = (): UserValueAtom | undefined => {
+  const currentUser = localStorage.getItem(CURRENT_USER_STORAGE_KEY)
+  if (isNil(currentUser)) {
+    return undefined
+  }
+  return JSON.parse(currentUser) as UserValueAtom
+}
+
+export const setUserStorage = (user: UserValueAtom) => {
+  localStorage.setItem(CURRENT_USER_STORAGE_KEY, JSON.stringify(user))
+}

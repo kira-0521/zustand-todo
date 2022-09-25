df4d128e1ced181060040ce1cd599af82e55e763
kira-0521 <kiratanaka0521@gmail.com>
2022-09-25 11:16:01 +0900

    [ fix ]

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 
 import { Home } from './components/Home'
 import { LoginForm } from './components/LoginForm'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   )
 }
 

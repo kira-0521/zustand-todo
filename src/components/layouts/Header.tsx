e6553a00d77369083b1e413b9023a1c7fb533925
kira-0521 <kiratanaka0521@gmail.com>
2022-09-25 14:38:03 +0900

    [ add ] username

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,7 +2,7 @@ import { useLogin } from '../../hooks/useLogin'
 import { useNavigate } from 'react-router-dom'
 
 export const Header = () => {
-  const { isLogin, onClickLogout } = useLogin()
+  const { currentUser, isLogin, onClickLogout } = useLogin()
 
   const navigate = useNavigate()
 
@@ -10,22 +10,25 @@ export const Header = () => {
     <header className="flex items-center justify-end p-6">
       <button onClick={isLogin ? onClickLogout : () => navigate('/')}>
         {isLogin ? (
-          <div className="flex gap-x-2 text-gray-500">
-            <span>Logout</span>
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              />
-            </svg>
+          <div className="flex flex-col gap-x-2 text-gray-500">
+            <div className="flex">
+              <span>Logout</span>
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+            </div>
+            <span className="text-neutral-700">{currentUser.name}</span>
           </div>
         ) : (
           <div className="flex gap-x-2 text-gray-500">
